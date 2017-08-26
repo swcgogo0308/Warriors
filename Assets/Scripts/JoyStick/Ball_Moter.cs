57aefd1706fa4db149fcdc6824a571d55b3de212
swcgogo0308 <swcgogo0308@naver.com>
2017-08-26 11:31:53 +0900

    Joystick
    조이스틱 가져옴

---
+++ Assets/Scripts/JoyStick/Ball_Moter.cs
@@ -0,0 +1,48 @@
+﻿//공을 움직이는 코드
+using UnityEngine;
+using System.Collections;
+
+public class Ball_Moter : MonoBehaviour
+{
+    public float moveSpeed = 10.0f;
+    public float drag = 0.5f;
+    public Vector3 MoveVector { set; get; }
+    public VirtualJoystick joystick;
+
+    private Rigidbody2D thisRigidbody;
+
+    void Start()
+    {
+        thisRigidbody = gameObject.AddComponent<Rigidbody2D>();
+        thisRigidbody = GetComponent<Rigidbody2D>();
+        thisRigidbody.drag = drag;
+    }
+
+    private void Update()
+    {
+        MoveVector = PoolInput();
+
+        Move();
+    }
+
+    private void Move()
+    {
+        thisRigidbody.velocity = MoveVector * moveSpeed;
+    }
+
+    private Vector3 PoolInput()
+    {
+        Vector3 dir = Vector3.zero;
+
+        //dir.x= Input.GetAxis("Horizontal");
+        //dir.z= Input.GetAxis("vertical");
+
+        dir.x = joystick.Horizontal();
+        dir.y = joystick.Vertical();
+
+        if (dir.magnitude > 1)
+            dir.Normalize();
+
+        return dir;
+    }
+}
---
+++ Assets/Scripts/Manager/Score.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class Score : MonoBehaviour {
+    public int scorePoint = 0;
+
+    private Text scoreTx;
+
+    void Awake()
+    {
+        scoreTx = GameObject.Find("Panel").transform.Find("ScoreTx").GetComponent<Text>();
+        scoreTx.text = "버틴시간 : 0";
+        StartCoroutine("PlusScoreRoutine");
+    }
+
+    public void PlusScore(int plusPoint)
+    {
+        scorePoint += plusPoint;
+        scoreTx.text = "버틴시간 : " + scorePoint.ToString();
+    }
+    IEnumerator PlusScoreRoutine()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(1.0f);
+            PlusScore(1);
+        }
+    }
+}
