864935c4a1a1d88d61f558d6313facc75c2e5672
hasechan <gktpcks401@naver.com>
2017-09-11 20:57:39 +0900

    main
    main

---
+++ Assets/Main/Fade.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fade : MonoBehaviour
+{
+
+    public void FadeMe()
+    {
+        StartCoroutine(DoFade());
+
+    }
+    IEnumerator DoFade()
+    {
+        CanvasGroup canvasGrop = GetComponent<CanvasGroup>();
+        while (canvasGrop.alpha > 0)
+        {
+            canvasGrop.alpha -= Time.deltaTime / 2;
+            yield return null;
+        }
+        canvasGrop.interactable = false;
+        yield return null;
+    }
+}
