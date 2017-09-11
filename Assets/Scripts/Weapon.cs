b443a3bc8bfd1b1fa8fa6ff62a3664a849328aff
swcgogo0308 <swcgogo0308@naver.com>
2017-09-11 14:40:31 +0900

    AttackAnimation Patch

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -34,6 +34,9 @@ public class Weapon : MonoBehaviour {
 
 	public bool _isBlocking;
 
+	bool _isAttackAni;
+
+
 
     // Use this for initialization
     void Start()
@@ -121,12 +124,11 @@ public class Weapon : MonoBehaviour {
     {
         if (_isAttacking) yield break;
 
-        _isAttacking = true;
-
-        yield return new WaitForSeconds(attackDelay);
+        //yield return new WaitForSeconds(attackDelay);
 
         if (isDead) yield break;
 
+		_isAttacking = true;
         weaponAni.SetBool("isAttacking", true);
         yield return new WaitForSeconds(1f);
         weaponAni.SetBool("isAttacking", false);
