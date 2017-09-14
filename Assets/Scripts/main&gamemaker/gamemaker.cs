8a0bfe4bc97f6d91edf100d38d2262f90fccffc7
hasechan <gktpcks401@naver.com>
2017-09-14 13:45:18 +0900

    main&gamemaker

---
+++ Assets/Scripts/main&gamemaker/gamemaker.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gamemaker : MonoBehaviour {
+
+    public void SceneTrans1()
+    {
+        Application.LoadLevel("Logo");
+    }
+    public void SceneTrans2()
+    {
+        Application.LoadLevel("Main");
+    }
+}
